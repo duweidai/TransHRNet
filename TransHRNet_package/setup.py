a350ef825c72ab83c0db8934085ecef2b9667f9c
duweidai <39508888+duweidai@users.noreply.github.com>
2022-10-22 16:45:28 +0800

    Add files via upload

---
+++ TransHRNet_package/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_namespace_packages
+
+setup(name='TransHRNet',
+      packages=find_namespace_packages(include=["TransHRNet", "TransHRNet.*"]),
+      version='0.0.1'
+      )
